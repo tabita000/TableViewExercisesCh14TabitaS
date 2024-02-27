e526a6d55782adb947128546a4bd00e7e9b8f679
Tabita Sadiq <tfadiq@students.mchenry.edu>
2024-02-27 17:44:14 -0600

    Initial Commit

---
+++ TableViewExercisesCh14TabitaS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewExercisesCh14TabitaS
+//
+//  Created by Tabita Sadiq on 2/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
