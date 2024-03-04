7c169f1062e4caa96b9b0971aee1146037331dbd
Tabita Sadiq <tfadiq@students.mchenry.edu>
2024-03-03 23:08:56 -0600

    Table View

--- TableViewExercisesCh14TabitaS/ViewController.swift
+++ TableViewExercisesCh14TabitaS/ViewController.swift
@@ -7,11 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    //UIViewController--This is the backbone of the screens in any app. Every screen in the app, where you interact with, or display content to the user is managed by this.
+    //UITableViewDelegate--this is used when you have a table view on your screen and you want to customize how it behaves when the user interacts with it.
+    //UITableViewDataSource--this is very important when table views are included on the screen. This is used to specify what data is displayed in the table.
+    
+    let petArray = ["cat", "dog", "parakeet", "parrot", "canary", "hamster", "horses", "cow", "fish", "monkey", "turtle", "leopard", "cheetah", "lion", "puppy", "kitten", "finch", "gerbil", "sean", "pig", "lama"]
+    
+    let cellID = "cellID"
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petArray.count
+    
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellID)
+        if (cell == nil) {
+            cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: cellID)
+        }
+        
+        cell?.textLabel?.text = petArray[indexPath.row]
+        return cell!
+        
+    }
+    
+    
+    @IBOutlet weak var petTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        petTable.dataSource = self
+        petTable.delegate = self
     }
 
 
